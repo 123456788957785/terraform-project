9ad69419770ff8338aab1469526b364fe328fc88
Kumar <137216668+123456788957785@users.noreply.github.com>
2023-08-23 01:10:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "terraform123uihjudhkjdqekjjkd"
+  bucket = "abcd1345ygujhbygjhbmnkjuijk"
 }
 
 resource "aws_iam_user" "seven" {
